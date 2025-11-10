43c3608d7adfaf28babdc7a016caebc999150af6
eala-lah <eala-lah@student.hive.fi>
2025-11-10 15:18:20 +0200

    manually creating desktop entry for kitty installation.

--- .setup.sh
+++ .setup.sh
@@ -101,6 +101,13 @@ if [ "$IS_TERMUX" = false ]; then
 			curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
 			ln -sf "$HOME/.local/kitty.app/bin/kitty" "$HOME/.local/bin/kitty"
 			ln -sf "$HOME/.local/kitty.app/bin/kitten" "$HOME/.local/bin/kitten"
+			
+			# Desktop Integration for Local Install
+			print_info "Integrating local Kitty with desktop environment..."
+			mkdir -p "$HOME/.local/share/applications"
+			cp "$HOME/.local/kitty.app/share/applications/kitty.desktop" "$HOME/.local/share/applications/" 2>/dev/null
+			sed -i "s|Icon=kitty|Icon=$HOME/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" "$HOME/.local/share/applications/kitty.desktop" 2>/dev/null
+			sed -i "s|Exec=kitty|Exec=$HOME/.local/bin/kitty|g" "$HOME/.local/share/applications/kitty.desktop" 2>/dev/null
 		else
 			print_error "curl missing. Cannot install Kitty fallback."
 		fi
